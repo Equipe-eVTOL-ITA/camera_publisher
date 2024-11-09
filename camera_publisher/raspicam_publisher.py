dde77e8dd161c954ffc2b568ad12a7a15785c44c
eVTOL-ITA <evtol.ita@gmail.com>
2024-11-09 20:09:17 -0300

    changed to compressed imgs again and standardized topic name

--- camera_publisher/raspicam_publisher.py
+++ camera_publisher/raspicam_publisher.py
@@ -1,13 +1,13 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge
 import cv2
 
 class CameraPublisher(Node):
     def __init__(self):
         super().__init__('camera_publisher')
-        self.publisher_ = self.create_publisher(Image, 'raspicam/image', 10)
+        self.publisher_ = self.create_publisher(CompressedImage, 'camera/image/compressed', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
         self.bridge = CvBridge()
         self.cap = cv2.VideoCapture('/dev/video0', cv2.CAP_V4L)
@@ -15,8 +15,8 @@ class CameraPublisher(Node):
     def timer_callback(self):
         ret, frame = self.cap.read()
         if ret:
-            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            msg = self.bridge.cv2_to_imgmsg(frame_rgb, encoding='rgb8')
+            #frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            msg = self.bridge.cv2_to_compressed_imgmsg(frame) # Using raw img: (frame_rgb, encoding='rgb8')
             self.publisher_.publish(msg)
 
 def main(args=None):
